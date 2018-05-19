485b4067f7b3bbf897b2512cac7b2dd32e70c689
Joshua Achorn <wight@cock.li>
2018-05-19 17:11:40 -0400

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,13 @@ module.exports = {
   entry: [
     "./bundler.js"
   ],
-  module: {
-    rules: []
-  },
   resolve: {
     extensions: ["*", ".js", ".jsx"]
   },
   output: {
     path:     path.join(__dirname, "dist"),
-    filename: "bundle.js"
-  },
-  plugins: [],
-  devServer: {
-    contentBase: "./dist"
+    filename: "bundle.js",
+    libraryTarget: 'var',
+    library: "Phoxy"
   }
-};
+}
